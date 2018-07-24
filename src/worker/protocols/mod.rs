1ef8a461b03cc9edd51d4dd4e7f5de58f923dd5f
dash83 <caballeromarco@gmail.com>
2018-07-24 10:41:24 +0100

    Feature complete
    The basic 2-radio support is in. This means that both radios are loaded from configuration (if they exist), passed to the protocol on creation and then start their respective listeners on their own thread. Protocols that use only 1 radio don't have to care about the 2nd radio support.
    
    All tests are passing, but no new tests have been added to stress the inner-workings of a protocol that actually uses both radios. This will be added in an upcoming feature branch.

--- src/worker/protocols/mod.rs
+++ src/worker/protocols/mod.rs
@@ -3,11 +3,15 @@
 //! struct to be accepted as a protocol to be run by meshsim.
 
 extern crate rand;
+extern crate toml;
+extern crate rustc_serialize;
 
-use worker::{MessageHeader, WorkerError, Radio};
+use worker::{MessageHeader, WorkerError, Radio };
+use worker::listener::*;
 use self::tmembership::TMembership;
 use std;
 use std::sync::Arc;
+use std::str::FromStr;
 
 pub mod tmembership;
 
@@ -24,16 +28,49 @@ pub trait Protocol : std::fmt::Debug + Send + Sync {
 }
 
 ///Current list of supported protocols by MeshSim.
+#[derive(Debug, Deserialize, Serialize, PartialEq, Clone)]
 pub enum Protocols {
     ///ToyMembership protocol implemented in order to test and develop MeshSim.
     TMembership,
 }
 
+impl FromStr for Protocols {
+    type Err = WorkerError;
+
+    fn from_str(s : &str) -> Result<Protocols, WorkerError> {
+        match s.to_uppercase().as_str() {
+            "TMEMBERSHIP" => Ok(Protocols::TMembership),
+            _ => Err(WorkerError::Configuration(String::from("The specified protocol is not supported."))),
+        }
+    }
+}
+
+/// Helper struct for the return type of build_protocol_resources().
+/// It contains all the protocol-releated resources necessary for the worker to do it's job.
+pub struct ProtocolResources {
+    ///Protocol handler for the selected protocol.
+    pub handler : Arc<Box<Protocol>>,
+    ///Collection of listeners ready to read network messages for their respective radios.
+    pub listeners : Vec<Option<Box<Listener>>>,
+}
+
 /// Provides a new boxed reference to the struct matching the passed protocol.
-pub fn build_protocol_handler( p : Protocols, sr : Arc<Radio>, seed : u32 ) -> Box<Protocol> {
+pub fn build_protocol_resources( p : Protocols, 
+                                 short_radio : Option<Arc<Radio>>,
+                                 long_radio : Option<Arc<Radio>>,
+                                 seed : u32 ) -> Result<ProtocolResources, WorkerError> {
     match p {
-        Protocols::TMembership => { 
-            Box::new(TMembership::new(sr, seed))
+        Protocols::TMembership => {
+            //Obtain the short-range radio. For this protocol, the long-range radio is ignored.
+            let sr = short_radio.expect("The TMembership protocol requires a short_radio to be provided.");
+            //Initialize the radio.
+            let listener = try!(sr.init());
+            let handler : Arc<Box<Protocol>> = Arc::new(Box::new(TMembership::new(sr, seed)));
+            let mut listeners = Vec::new();
+            listeners.push(Some(listener));
+            let resources = ProtocolResources{  handler : handler, 
+                                                listeners : listeners };
+            Ok(resources)
         }
     }
 }
