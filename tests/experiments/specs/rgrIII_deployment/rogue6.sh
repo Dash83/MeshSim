e55c8d865fc3e74d93fea3c5defbfbd36a54de33
Marco Caballero <Dash83@users.noreply.github.com>
2019-09-17 18:11:29 +0100

    Update to rogue6.sh
    Patching the script with an endless loop such that the main thread of worker_cli does not terminate.

--- tests/experiments/specs/rgrIII_deployment/rogue6.sh
+++ tests/experiments/specs/rgrIII_deployment/rogue6.sh
@@ -1,1 +1,1 @@
-(sleep 10; echo "SEND rogue9 aGVsbG8gZnJvbSByb2d1ZTYK"; sleep 60) | ./target/release/worker_cli --config tests/experiments/specs/rgrIII_deployment/rogue6.toml --work_dir results/rgrIII_deployment --register_worker false --accept_commands true --log_to_terminal true
+(sleep 10; echo "SEND rogue9 aGVsbG8gZnJvbSByb2d1ZTYK"; while :; do sleep 1; done) | ./target/release/worker_cli --config tests/experiments/specs/rgrIII_deployment/rogue6.toml --work_dir results/rgrIII_deployment --register_worker false --accept_commands true --log_to_terminal true
